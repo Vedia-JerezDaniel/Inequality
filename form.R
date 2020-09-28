39ff9d02b41bbbfc7790e29c16459595350de373
DaniZ <danielvediaj@gmail.com>
2020-09-28 19:37:11 +0200

    From Z 1

---
+++ form.R
@@ -0,0 +1,35 @@
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+  + Gov_ex + lag(diff(Net_len_wi))
+  
+## INST
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+           + Gov_ex + ((goveff + instdepth + insaccess + ins_effic))
+
+#KIND EXPE
+pfe <- plm(Gini_net ~ lag(diff(GDP_con))  + lag(Trade) + 
+      Gov_ex + diff(Health_exp) , data = ine, model = "within", index = 'idem')
+
+pfe <- plm(Gini_net ~ lag(diff(GDP_con))  + lag(Trade) + Gov_ex 
+           + (diff(Education_exp)
+            
+# TAXES
+            
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+   + Gov_ex + (ind_tx*arp_mid)
+   
+# SOC TRANS
+
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+   + ((Social_prot))
+   
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+   + ((Social_prot_ex_pen))
+   
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+              + Gov_ex + (diff(Soc_kind))
+           
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+      + Gov_ex + (lag(Soc_payable))
+
+pfe <- plm(Gini_net ~ lag(diff(GDP_con)) + lag(Secondary_ed) + lag(Trade)
+      + Gov_ex + ((Soc_payable))
